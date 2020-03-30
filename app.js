3cc921be2b8db61882483e9857bf513507c5182d
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-03-30 23:26:22 +0200

    removed not important files and texts

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ var accountRouter = require('./routes/account');
 var productRouter = require('./routes/product');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
