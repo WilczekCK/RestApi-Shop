0228517664e06641990345ec1dec1af3bb9a86df
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-03-31 14:26:03 +0200

    basic query working

--- controllers/mysql_controler.js
+++ controllers/mysql_controler.js
@@ -3,21 +3,21 @@ var mysql = require('mysql');
 var mysql_controler = mysql_controler || {}
 mysql_controler = {
     auth: {
+        host: 'localhost',
         user: 'root',
         password: 'rootpass',
         database: 'fastsell',
-        host: 'localhost'
     },
-    connection: () => mysql.createConnection(mysql_controler.auth),
     query: () => {
-        mysql_controler.connection.connect();
+        var connection = mysql.createConnection(mysql_controler.auth);
+        connection.connect();
 
         connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
             if (err) throw err  
             console.log('The solution is: ', rows[0].solution)
         })
 
-        mysql_controler.connection.end()
+        connection.end()
     },
     insert: (table, rowNames, rowsInfo) => {},
     update: (table, changingRows, replacedRows) => {},
