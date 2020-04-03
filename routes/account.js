b7c62d47f920e1be0d78035bfb80ac6b498ecc11
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-04-03 15:51:15 +0200

    Revert "changed account route"
    This reverts commit fe6a61fbe519f0e8c49065c67607efce99a8f82e.

--- routes/account.js
+++ routes/account.js
@@ -1,25 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('../controllers/mysql_controler.js');
 
-router.get('/login', function(req, res, next) {
-    res.status(200).end()
+router.get('/', function(req, res, next) {
+    //console.log(await mysql.query('select * from test'))
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
 
-router.post('/login', function(req, res, next) {
-    res.status(200).end()
-});
+router.post('/register', function(req, res, next) {});
+router.get('/login', function(req, res, next) {});
 
-router.get('/register', function(req, res, next) {
-    res.status(200).end()
-});
+router.get('/details', function(req, res, next) {}); //get
+router.post('/details', function(req, res, next) {}); //changing
 
-router.post('/register', function(req, res, next) {
-    res.status(200).end()
-});
-
-router.post('/delete', function(req, res, next) {
-    res.status(200).end()
-});
-
+router.delete('/delete', function(req, res, next) {});
 
 module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -5,4 +5,23 @@ router.get('/', function(req, res, next) {
     res.status(200).end()
 });
 
+router.post('/', function(req, res, next) {
+    res.status(200).end()
+});
+
+router.delete('/', function(req, res, next) {
+    res.status(200).end()
+});
+
+router.patch('/details', function(req, res, next) {
+    //route to change details
+    res.status(200).end()
+});
+
+router.get('/:url_name', function(req, res, next) {
+    //urlname from db
+    res.status(200).end()
+});
+
+
 module.exports = router;
