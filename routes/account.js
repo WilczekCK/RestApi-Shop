bf30ff9de2c731334d244d8486c211f0372814f4
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-04-03 15:45:09 +0200

    just account route

--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 router.get('/login', function(req, res, next) {
     res.status(200).end()
 });
@@ -17,12 +16,10 @@ router.get('/register', function(req, res, next) {
 router.post('/register', function(req, res, next) {
     res.status(200).end()
 });
-=======
-router.get('/', function(req, res, next) {
-    //console.log(await mysql.query('select * from test'))
-    mysql.insert('test', 'id', 1);
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+
+router.post('/delete', function(req, res, next) {
+    res.status(200).end()
 });
 
->>>>>>> parent of d7ddc8d... all routes decided to be
+
 module.exports = router;
