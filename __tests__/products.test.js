9915bc7462739995de88db0c590bce54f92611fb
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-04-14 13:05:12 +0200

    first test working

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -1,5 +1,6 @@
 //routes.test.js
 const request = require('supertest');
+const http = require('http');
 const server = require('../app.js');
 
 beforeAll(async () => {
@@ -12,9 +13,13 @@ afterAll(async (done) => {
 })
 
 describe('Route tests', () => {
-    test('Is product route available?  "/product" ', async () => {
+    test('Is product display route available?  "GET /product" ', async () => {
         const response = await request(server).get('/product');
         expect(response.status).toEqual(200);
+    });
+
+    test('Is product possible to add?  "POST /product" ', async () => {
+
     });
 
 });
