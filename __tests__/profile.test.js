645ca92714de4ab55cb9888be811d1fa68943a9c
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-05-11 16:45:37 +0200

    login/register tests

---
+++ __tests__/profile.test.js
@@ -0,0 +1,44 @@
+//routes.test.js
+const request = require('supertest');
+const http = require('http');
+const server = require('../app.js');
+
+beforeAll(async () => {
+    // do something before anything else runs
+    console.log('Jest starting!');
+});
+
+afterAll(async (done) => {
+    console.log('Jest closing!')
+    server.close(done);
+})
+
+describe('Profile test', () => {
+    test('Is profile possible to add?  "POST account/register" ', async () => {
+        await request(server)
+            .post('/account/register')
+            .send({
+            firstName: 'Adam',
+            secondName: 'Kowalsky',
+            email: 'adam.kowalsky@gmail.com',
+            password: 'TestingPurposes12!',
+            phone: 231213421,
+            street: 'Warszawska 12',
+            city: 'Cracow',
+            postCode: '21-231'
+            })
+            .expect(200);
+    });
+
+    test('Is added profile possible to login "POST account/login"', async () => {
+        await request(server)
+            .post('/account/login')
+            .send({
+                username: 'adam.kowalsky@gmail.com',
+                password: 'TestingPurposes12!'
+            })
+            .expect(200)
+    })
+
+
+});
