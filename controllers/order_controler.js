90671df1cc80f7b0b51fe99e52aa66c6df83e218
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-05-13 15:13:03 +0200

    restructure

--- controllers/order_controler.js
+++ controllers/order_controler.js
@@ -3,15 +3,15 @@ var _ = require('underscore');
 
 var order_controler = order_controler || {}
 order_controler = {
-    onlinePayment:{
-        preparePayment: () => {},
-        setPaid: () => {}
+    onlinePayment: {
+        preparePayment: () => { },
+        setPaid: () => { }
     },
-    createOrder: async ({customerId, productsOrdered}) => {
-        if(!customerId || !productsOrdered) return {status: 400, message:'You are missing one of the parameters'};
+    createOrder: async ({ customerId, productsOrdered }) => {
+        if (!customerId || !productsOrdered) return { status: 400, message: 'You are missing one of the parameters' };
         const summaryPrice = await order_controler.sumPrice(productsOrdered);
 
-        return {status: summaryPrice.status, customerId: customerId, productsOrdered: productsOrdered, summaryPrice: summaryPrice.message};
+        return { status: summaryPrice.status, customerId: customerId, productsOrdered: productsOrdered, summaryPrice: summaryPrice.message };
     },
     removeOrder: () => {
 
@@ -19,23 +19,39 @@ order_controler = {
     changeAmount: () => {
 
     },
-    sumPrice: async (productsOrdered) => {
-        try{
-            const productsInfo = await mysql.showCertain('products', 'price, name, vat_percentage', `id IN (${productsOrdered})`)
-            let priceSummary = 0;
-            
-            _.each(productsInfo, async ({price, name, vat_percentage}) => {
-                //without percentage included
-                return priceSummary = priceSummary + price;
+    prepareArray: async (productsOrdered) => {
+        try {
+            let products = {
+                idArray: [],
+                amountArray: [],
+                priceArray: [],
+                priceSummary: 0
+            };
+
+            _.each(productsOrdered, async ({productId, amount}) => {
+                products.idArray.push(productId);
+                products.amountArray.push(amount);
             })
-    
-            return {status: 200, message: {withoutVat: priceSummary, withVat: ((priceSummary) * 1.23)}};
-        }catch(error){
-            return {status: 406, message:'Error while summary of products!'}
+
+            const productsInfo = await mysql.showCertain('products', 'price', `id IN (${products.idArray})`)
+            _.each(productsInfo, async ({price}) => products.priceArray.push(price));
+
+            return products;
+        } catch (error) {
+            return { status: 406, message: 'Error while creating table for summary' }
+        }
+    },
+    sumPrice: async (productsOrdered) => {
+        try {
+            const products = await order_controler.prepareArray(productsOrdered);
+            console.log(products)
+            //return { status: 200, message: { withoutVat: priceSummary, withVat: ((priceSummary) * 1.23) } };
+        } catch (error) {
+            //return { status: 406, message: 'Error while summary of products!' }
         }
     },
     setStatus: (status) => {
-        
+
     }
 }
 
