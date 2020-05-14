be5bfa0402ddd6b06b0857b456ee3a01e9dc8029
Szpakusik <szpakusik@gmail.com>
2020-05-14 13:58:40 +0200

    profile auth + test auth fix

--- routes/account.js
+++ routes/account.js
@@ -5,6 +5,8 @@ var profile = require('../controllers/profile_controler.js');
 var auth = require('../controllers/auth_controler.js');
 const passport = require('passport');
 
+let userId;
+
 router.get('/', function (req, res, next) {
   //console.log(await mysql.query('select * from test'))
   res.render('index', { title: 'Hey', message: 'Hello there!' })
@@ -24,16 +26,20 @@ router.post('/login', async (req, res, next) => {
 router.get('/details', function (req, res, next) { }); //get
 
 router.patch('/details', async function (req, res, next) {
-  const changeResponse = await profile.changeInfo(req.body);
-  res.status(changeResponse.status).send(changeResponse);
+  await auth.authenticate(req, res).then( async(userID)=> {
+    const changeResponse = await profile.changeInfo(req.body);
+    res.status(changeResponse.status).send(changeResponse);
+  }, err => console.log(err) )
 });
 
 router.delete('/delete', async function (req, res, next) {
-  const deletionResponse = await profile.remove(req.body);
-  res.status(deletionResponse.status).send(deletionResponse);
+  await auth.authenticate(req, res).then( async(userID)=> {
+    const deletionResponse = await profile.remove(req.body);
+    res.status(deletionResponse.status).send(deletionResponse);
+  }, err => console.log(err) )  
 });
 
-router.get('/findUser', async(req, res, next) => {
+router.get('/details', async(req, res, next) => {
   auth.authenticate(req, res, next).then(async(userId)=> {
     const response = await profile.lookForProfile(`id = ${userId}`)
     res.status(200).send(response);
