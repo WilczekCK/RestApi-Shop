c62e58aded220de9c93f3bcf0e12a741cf224b28
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-05-19 14:06:50 +0200

    new way of fromUser array response

--- controllers/order_controler.js
+++ controllers/order_controler.js
@@ -9,26 +9,30 @@ order_controler = {
         setPaid: () => { }
     },
     display: {
-        all: async ({limit}) => {
-            if(_.isNumber(limit)) limit = `LIMIT ${limit}`
+        all: async ({ limit }) => {
+            if (_.isNumber(limit)) limit = `LIMIT ${limit}`
             else limit = '';
 
             const orderRecords = await mysql.show(`orders, order_detail ORDER BY orders.date DESC ${limit}`, `orders.*, order_detail.*`);
-            return {status: 200, orders: orderRecords}
+            return { status: 200, orders: orderRecords }
         },
-        fromUser: async ({user_id, limit}) => {
-            if(!user_id) return {status: 406, message:'You are missing one of the parameters'};
-            if(_.isNumber(limit)) limit = `LIMIT ${limit}`
+        fromUser: async ({ user_id, limit }) => {
+            if (!user_id) return { status: 406, message: 'You are missing one of the parameters' };
+            if (_.isNumber(limit)) limit = `LIMIT ${limit}`
             else limit = '';
 
-            const orderRecords = await mysql.showCertain('orders, order_detail', 'orders.*, order_detail.*', `orders.user_id = ${user_id} AND orders.id = order_detail.order_id ORDER BY orders.date DESC ${limit}` );
-            return {status: 200, orders: orderRecords}
-        },
-        single: async ({order_id}) => {
-            if(!order_id) return {status: 406, message:'You are missing one of the parameters'};
+            const orderRecords = await mysql.showCertain('orders, order_detail', 'orders.*, order_detail.*', `orders.user_id = ${user_id} AND orders.id = order_detail.order_id ORDER BY orders.date DESC ${limit}`);
+            if(!orderRecords.length) return {status: 404, message: 'User not found in db'};
 
-            const orderRecords = await mysql.showCertain('orders, order_detail', 'orders.*, order_detail.*', `orders.id = ${order_id} AND order_detail.order_id = ${order_id} ORDER BY orders.date DESC` );
-            return {status: 200, orders: orderRecords}
+            const shuffledRecords = await order_controler.getOrdersArrayFromUser(orderRecords);
+
+            return { status: 200, orders: shuffledRecords }
+        },
+        single: async ({ order_id }) => {
+            if (!order_id) return { status: 406, message: 'You are missing one of the parameters' };
+
+            const orderRecords = await mysql.showCertain('orders, order_detail', 'orders.*, order_detail.*', `orders.id = ${order_id} AND order_detail.order_id = ${order_id} ORDER BY orders.date DESC`);
+            return { status: 200, orders: orderRecords }
         }
     },
     createOrder: async ({ customerId, productsOrdered }) => {
@@ -69,7 +73,24 @@ order_controler = {
             return { status: 200, message: 'You removed the order successfully!' }
         }
     },
-    prepareArray: async (productsOrdered) => {
+    getOrdersArrayFromUser: async (orders) => {
+        const productsOrdered = []; 
+
+        _.each(_.uniq(_.pluck(orders, 'id')), order_id => {
+            productsOrdered.push({order_id: order_id, products: []})
+        })
+
+        orders.forEach(order => {
+            let getOrderIdPosition = _.indexOf(_.pluck(productsOrdered, 'order_id'), order.id);
+            productsOrdered[getOrderIdPosition].products.push({
+                product_id: order.product_id,
+                amount: order.amount
+            })
+        })
+
+        return productsOrdered;
+    },
+    prepareOrderArray: async (productsOrdered) => {
         try {
             const productsInfo = await mysql.showCertain('products', 'price', `id IN (${_.pluck(productsOrdered, 'productId')})`)
             let products = {
@@ -91,7 +112,7 @@ order_controler = {
     },
     sumPrice: async (productsOrdered) => {
         try {
-            const { idArray, priceArray, amountArray } = await order_controler.prepareArray(productsOrdered);
+            const { idArray, priceArray, amountArray } = await order_controler.prepareOrderArray(productsOrdered);
             var priceSummary = 0;
 
             let queue = 0;
@@ -106,11 +127,11 @@ order_controler = {
             return { status: 406, message: 'Error while summary of products!' }
         }
     },
-    setStatus: async ( {status, id, user_id} ) => {
-        if(!status || !id || !user_id) return { status: 406, message: 'You are missing one of the parameters!' }
+    setStatus: async ({ status, id, user_id }) => {
+        if (!status || !id || !user_id) return { status: 406, message: 'You are missing one of the parameters!' }
 
         const response = await mysql.update('orders', `status = ${status}`, `id = ${id} AND user_id = ${user_id}`);
-        if(response) return { status: 200, message: 'You changed the order successfully!' }
+        if (response) return { status: 200, message: 'You changed the order successfully!' }
         else return { status: 406, message: 'Error while setting status!' }
     }
 }
