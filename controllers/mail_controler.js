af320597f50b2391c8ec533c45712ec6ece95e7a
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-06-04 16:11:18 +0200

    mail available to send

--- controllers/mail_controler.js
+++ controllers/mail_controler.js
@@ -2,21 +2,34 @@ var mysql = require('./mysql_controler');
 var nodemailer = require('nodemailer');
 var _ = require('underscore');
 
-
 var mail_controler = mail_controler || {}
 mail_controler = {
-    config:{
-            host: 'smtp.mailtrap.io',
-            port: 465,
-            secure: true,
-            auth:{
-                user:'c453680365e19b',
-                pass: 'b0dc723f60997f',
-            }
-    },
-    transporter: () => nodemailer.createTransport(mail_controler.config),
+    transporter: nodemailer.createTransport({
+        host: 'smtp.mailtrap.io',
+        port: 587,
+        secure: false,
+        auth:{
+            user:'c453680365e19b',
+            pass: 'b0dc723f60997f',
+        }
+    }),
     send: async (deliverTo, subject, text, html) => {
-        await transporter
+        try{
+            const callback = await mail_controler.transporter.sendMail({
+                from: 'Bryan - Your shop assistent <noreply@z-dowozem.com> |',
+                to: deliverTo,
+                subject: subject,
+                text: text,
+                html: html
+            })
+
+            if(callback.accepted) return {status: 'success'}
+            else if(callback.rejected) return {status: 'deny'}
+            else return {status: undefined}
+        }catch{
+            return new Error('There is a problem with sending the mail!')
+        }
+
     }
 }
 
