58ef7f917556268c7b2650102861b0e5625ba188
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-06-04 15:06:46 +0200

    mail controler init + mailer npm

---
+++ controllers/mail_controler.js
@@ -0,0 +1,12 @@
+var mysql = require('./mysql_controler');
+var nodemailer = require('nodemailer');
+var _ = require('underscore');
+
+
+var mail_controler = mail_controler || {}
+mail_controler = {
+
+}
+
+
+module.exports = mail_controler || 'Mail Controler Problem!';
