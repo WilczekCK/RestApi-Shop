00d9d012b5b7b04e7e4a4a18e87e741ea84afabe
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-06-04 15:20:40 +0200

    basic informations provided

--- controllers/mail_controler.js
+++ controllers/mail_controler.js
@@ -5,7 +5,19 @@ var _ = require('underscore');
 
 var mail_controler = mail_controler || {}
 mail_controler = {
-
+    config:{
+            host: 'smtp.mailtrap.io',
+            port: 465,
+            secure: true,
+            auth:{
+                user:'c453680365e19b',
+                pass: 'b0dc723f60997f',
+            }
+    },
+    transporter: () => nodemailer.createTransport(mail_controler.config),
+    send: async (deliverTo, subject, text, html) => {
+        await transporter
+    }
 }
 
 
